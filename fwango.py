3d80afed512b15b93c72abf52303cafa2ba4ea75
Tahseen Robbani <tahseenrobbani@EmilyElrsiPhone.localdomain>
2022-12-11 14:53:05 -0600

    can iterate and get stats, but problem of some tournaments with no dropdowns

--- fwango.py
+++ fwango.py
@@ -42,12 +42,11 @@ WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
 # card_cont = soup.find_all("a", class_="TournamentCard__Container-sc-15zkmn8-0")
 # find the 'next' button using xpath
     
-href_list = []
 current_page_num = "1"
 
 def retrieve_all_links():
     global current_page_num
-    global href_list
+    href_list = []
     next_button_class = ""
     while ("is-disabled" not in next_button_class):
         # find each tournament card with class starting with substring
@@ -66,63 +65,75 @@ def retrieve_all_links():
         else:
             break
         print(driver.current_url)
+    return href_list
         # WebDriverWait(driver, 20).until(EC.url_contains("page=" + current_page_num))
         # sleep(1)
 
-def retrieve_scores():
-    # for i in hrefs:
-    # driver.get(i)
-    driver.get("https://fwango.io/fallnashville2022")
-    print(driver.current_url)
-    # waits for results button i think?
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"root\"]/span/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/nav/ul[2]/li[2]/div/a")))
-    # click on results
-    driver.find_element(By.XPATH, "//*[@id=\"root\"]/span/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/nav/ul[2]/li[2]/div/a").click()
-    # wait until dropdown is located
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "select-input-container")))
-    # select the dropdown with skill divisions
-    driver.find_element(By.CSS_SELECTOR, "div[class='select-input-container']").click()
-    # wait for drop down options to load
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class$='menu']")))
-    menu = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
-    options_list = menu.find_elements(By.CSS_SELECTOR, "div[class$='option']")
-    print(len(options_list))
-    for i in range(0, len(options_list)):
-        print(i)
-        
-        # check to see if dropdown is still selected
-        try:
-            menu_temp = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
-        except NoSuchElementException:
-            driver.find_element(By.CSS_SELECTOR, "div[class='select-input-container']").click()
-            # wait for drop down options to load
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class$='menu']")))
-            menu_temp = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
+def retrieve_scores(hrefs):
+    for i in hrefs:
+        driver.get(i)
+        # driver.get("https://fwango.io/sswomensdouble")
+        print(driver.current_url)
+        # waits for results button i think?
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"root\"]/span/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/nav/ul[2]/li[2]/div/a")))
+        # click on results
+        driver.find_element(By.XPATH, "//*[@id=\"root\"]/span/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/nav/ul[2]/li[2]/div/a").click()
+        # wait until dropdown is located
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "select-input-container")))
+        # TODO: sometimes, there is no dropdown ugh - make special function for no dropdown
+        # select the dropdown with skill divisions
+        driver.find_element(By.CSS_SELECTOR, "div[class='select-input-container']").click()
+        # wait for drop down options to load
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class$='menu']")))
+        menu = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
+        options_list = menu.find_elements(By.CSS_SELECTOR, "div[class$='option']")
+        print(len(options_list))
+        for j in range(0, len(options_list)):
+            print(j)
+            # check to see if dropdown is still selected
+            try:
+                menu_temp = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
+            except NoSuchElementException:
+                driver.find_element(By.CSS_SELECTOR, "div[class='select-input-container']").click()
+                # wait for drop down options to load
+                WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class$='menu']")))
+                menu_temp = driver.find_element(By.CSS_SELECTOR, "div[class$='menu']")
+                
+            options_list_temp = menu_temp.find_elements(By.CSS_SELECTOR, "div[class$='option']")
+            options_list_temp[j].click()
             
-        options_list_temp = menu_temp.find_elements(By.CSS_SELECTOR, "div[class$='option']")
-        options_list_temp[i].click()
-        # find each team column
-        try:
-            WebDriverWait(driver, 6).until(EC.presence_of_element_located((By.CLASS_NAME, "team-column")))  
-            teams = driver.find_elements(By.CLASS_NAME, "team-column")
-        except TimeoutException:
-            continue
-        for i in teams[1:]:    
-            # for some reason, find_element wasn't working when applied on a webelement, so 
-            # using bs4 here to keep this data together for now
-            html = i.get_attribute('innerHTML')
-            soup = BeautifulSoup(html, 'html.parser')
-            team_name = soup.find('div', class_="team-name").get_text()
-            team_members = soup.find('div', class_="players").get_text()
-            team_members = team_members.split(" and ")
-            # print(team_name, team_members)
-            print(team_name)
-            print(team_members)
+            # TODO: save division name
+            
+            # find each team column
+            try:
+                # this is to check if that division has teams
+                WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "team-column")))  
+                teams = driver.find_elements(By.CLASS_NAME, "team-column")
+            except TimeoutException:
+                continue
+            # skip the first one bc it's the column title
+            for x in teams[1:]:     
+                # for some reason, find_element wasn't working when applied on a webelement, so 
+                # using bs4 here to keep this data together for now
+                html = x.get_attribute('innerHTML')
+                soup = BeautifulSoup(html, 'html.parser')
+                team_name = soup.find('div', class_="team-name").get_text()
+                team_members = soup.find('div', class_="players").get_text()
+                team_members = team_members.split(" and ")
+                # print(team_name, team_members)
+                print(team_name)
+                print(team_members)
+                x.click()
+                stats = driver.find_elements(By.CSS_SELECTOR, "div[class^='Statisticsstyle__StatisticsItem']")
+                for s in stats:
+                    value = s.find_element(By.CLASS_NAME, "value").text
+                    label = s.find_element(By.CLASS_NAME, "label").text
+                    print(value + " " + label)
     
 
 # retrieve_all_links()
-# retrieve_scores(href_list)
-retrieve_scores()
+retrieve_scores(retrieve_all_links())
+# retrieve_scores()
 # print(href_list)
 # for i in href_list:
 #     driver.get(i)
